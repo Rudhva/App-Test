44b359d7f5765ee91cfa5d3c67244b78bc13c36b
Rudhva <rpgenius12@icloud.com>
2023-06-25 13:45:52 -0500

    Rough Login/Signup Pages

--- App Test/ViewController.swift
+++ App Test/ViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import Contacts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func fetchAllContacts() {
+        
+        //get access to contacts store
+        let store = CNContactStore()
+        
+        //specify which data keys we want to fetch
+        let keys = {CNContactGivenNameKey, CNContactPhoneNumbersKey} as [CNKeyDescriptor]
+        
+        //Create fetch request
+        let fetchRequest = CNContactFetchRequest(keysToFetch: <#T##[CNKeyDescriptor]#>)
+        
+        //call method to fetch all contacts
+        do {
+        try store.enumerateContacts(with: <#T##CNContactFetchRequest#>, usingBlock: <#T##(CNContact, UnsafeMutablePointer<ObjCBool>) -> Void#>)
+        }
+        catch {
+            //if there is an error, handle it here
+        }
+        
+        
     }
 
 
